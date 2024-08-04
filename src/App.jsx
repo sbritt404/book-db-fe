59f066627308fbcc34688a89a6ea69f1ca01a057
sbritt404 <sbrittain.prsvr@gmail.com>
2024-08-03 21:12:42 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import BookList from './components/Book/BookList';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <BookList />
+    </div>
+  )
+}
+
+export default App;
